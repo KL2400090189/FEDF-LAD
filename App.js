4fe4745bd6a12d60979ebdcbdc80dfdd49d40155
KL2400090189 <2400090189@kluniversity.in>
2025-10-17 10:44:49 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import ProductFilter from './components/ProductFilter';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductFilter />
+    </div>
+  );
+}
+
+export default App;
